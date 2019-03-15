1c6da8ee17725fb9c4ff638440c2b74fccaff687
Dave Price <dave@daveprice.co.uk>
2019-03-15 16:19:54 +0000

    update of carousel

--- assets/js/raf.js
+++ assets/js/raf.js
@@ -1,33 +1,39 @@
 var progress = 0;
 var start = null;
-var j = 1;
+var j = 0;
 var k = 2;
 var carousel = document.getElementsByClassName('block');
 function step(timestamp) {
-
   if (!start) start = timestamp;
   var progress = timestamp - start;
 
   if (progress < 120) {
-    bannerSwitch();
     window.requestAnimationFrame(step);
+
+  }
+
+  if (progress >=120) {
+    console.log('dave');
+    if (j<2) {
+      if (j==0) {
+        bannerSwitch(1,2);
+      } else if (j==1) {
+        bannerSwitch(1,0);
+      }
+    } else {
+      bannerSwitch(0,2);
+      j=0;
+    }
 
   }
 
 }
 
-function bannerSwitch() {
-  console.log('This is j' + j, 'This is k' + k);
+function bannerSwitch(j,k) {
   carousel[j].classList.add('show');
   carousel[k].classList.remove('show');
+  window.requestAnimationFrame(step);
 
-  if (j==2) {
-    j=0;
-    k=j+2;
-  } else {
-    j++;
-    k=j-1;
-  }
 }
 
 window.requestAnimationFrame(step);
