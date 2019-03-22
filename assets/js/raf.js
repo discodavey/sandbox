28bd09ff3db4c291f77015d5bc65070b6a4e6e28
Dave Price <dave@daveprice.co.uk>
2019-03-22 13:41:05 +0000

    Updates for carousel and keyframes

--- assets/js/raf.js
+++ assets/js/raf.js
@@ -2,9 +2,9 @@ var progress = 0;
 var start = null;
 var j = 0;
 var k = 2;
-var timeSet = 2000;
+var timeSet = 4000;
 var carousel = document.getElementsByClassName('block');
-console.log('This is ' + j, k);
+//console.log('This is ' + j, k);
 
 function step(timestamp) {
   if (!start) start = timestamp;
@@ -12,7 +12,7 @@ function step(timestamp) {
   if (progress < timeSet) {
     window.requestAnimationFrame(step);
   }
-  if (progress >= timeSet) {
+  if (progress > timeSet) {
     if (j == 2) {
       j=0;
       k=2;
@@ -20,6 +20,7 @@ function step(timestamp) {
       j++;
       k=j-1;
     }
+
     console.log('This is ' + j, k);
     bannerSwitch(j,k);
     start = null;
@@ -28,8 +29,15 @@ function step(timestamp) {
 }
 
 function bannerSwitch(j,k) {
+  console.log(j,k);
   carousel[j].classList.add('show');
   carousel[k].classList.remove('show');
+  // var blockOne = document.querySelector('.block1');
+  var blockTwo = document.querySelector('.block2');
+  // var blockThree = document.querySelector('.block3');
+  // blockOne.classList.add('slide-first');
+  blockTwo.classList.add('slide-first');
+  // blockThree.classList.add('slide-first');
 }
 
 window.requestAnimationFrame(step);
