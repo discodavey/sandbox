cf7fbaacadb4a8991745d8509f1ab3f0cce026e8
Dave Price <d.price@elifesciences.org>
2023-09-18 10:40:15 +0100

    dialogue modal

---
+++ dialogue.js
@@ -0,0 +1,6 @@
+const niceBtn = document.getElementById('niceBtn');
+const niceModal = document.getElementById('niceModal');
+
+niceBtn.addEventListener('click', () => {
+  niceModal.showModal();
+})
